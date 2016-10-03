4582f7c8768dea7da8518dc0198d3981c7f917f3
Sheena Artrip <sheena.artrip@gmail.com>
2016-10-03 17:45:24 -0400

    Initial commit of working ariproxy.
    Uses the code in github.com/CyCoreSystems/ari commit 768a281 with
    minimal changes to name and build process.

---
+++ ariproxy/options.go
@@ -0,0 +1,16 @@
+package ariproxy
+
+import (
+	"golang.org/x/net/context"
+	log15 "gopkg.in/inconshreveable/log15.v2"
+)
+
+// Options are the group of options for the ari-proxy server
+type Options struct {
+	//TODO: include nats options
+
+	URL string
+
+	Logger log15.Logger
+	Parent context.Context
+}
--- cmd/ari-proxy/main.go
+++ cmd/ari-proxy/main.go
@@ -1,4 +1,7 @@
 package main
 
+import "github.com/CyCoreSystems/ari-proxy/cmd/ari-proxy/cmd"
+
 func main() {
+	cmd.Execute()
 }

01f0c810d9542126f9d32fcd2ba5f4dc9ed4e9a4
Sheena Artrip <sheena.artrip@gmail.com>
2017-02-19 22:01:09 -0500

    server - Replace entity tests and objects for ari.Entity

--- server/events.go
+++ server/events.go
@@ -2,66 +2,9 @@ package ariproxy
 
 import "github.com/CyCoreSystems/ari"
 
-type entity struct {
-	// Type is the type of entity
-	Type string
-
-	// ID is the unique identifier for the entity
-	ID string
-}
-
 func (s *Server) dialogsForEvent(e ari.Event) (ret []string) {
-	for _, i := range entitiesFromEvent(e) {
+	for _, i := range ari.EntitiesFromEvent(e) {
 		ret = append(ret, s.Dialog.List(i.Type, i.ID)...)
 	}
-	return
-}
-
-func entitiesFromEvent(e ari.Event) (ret []entity) {
-	if v, ok := e.(ari.ChannelEvent); ok {
-		for _, id := range v.GetChannelIDs() {
-			ret = append(ret, entity{
-				Type: "channel",
-				ID:   id,
-			})
-		}
-	}
-
-	if ev, ok := e.(ari.BridgeEvent); ok {
-		for _, id := range ev.GetBridgeIDs() {
-			ret = append(ret, entity{
-				Type: "bridge",
-				ID:   id,
-			})
-		}
-	}
-
-	if ev, ok := e.(ari.EndpointEvent); ok {
-		for _, id := range ev.GetEndpointIDs() {
-			ret = append(ret, entity{
-				Type: "endpoint",
-				ID:   id,
-			})
-		}
-	}
-
-	if ev, ok := e.(ari.PlaybackEvent); ok {
-		for _, id := range ev.GetPlaybackIDs() {
-			ret = append(ret, entity{
-				Type: "playback",
-				ID:   id,
-			})
-		}
-	}
-
-	if ev, ok := e.(ari.RecordingEvent); ok {
-		for _, id := range ev.GetRecordingIDs() {
-			ret = append(ret, entity{
-				Type: "recording",
-				ID:   id,
-			})
-		}
-	}
-
 	return
 }

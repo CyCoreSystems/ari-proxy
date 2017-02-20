9923ddf71e1d2b0d7fcf458e0a4f54ddfaa7312e
Seán C McCord <ulexus@gmail.com>
2017-02-19 20:41:52 -0500

    add first events test

---
+++ server/events_test.go
@@ -0,0 +1,29 @@
+package ariproxy
+
+import (
+	"testing"
+	"time"
+
+	"github.com/CyCoreSystems/ari"
+)
+
+func TestEntitiesFromChannelEvent(t *testing.T) {
+	e := ari.ChannelDtmfReceived{
+		EventData: ari.EventData{
+			Application: "testApp",
+			Timestamp:   ari.DateTime(time.Now()),
+		},
+		Channel: ChannelData{
+			ID:   "testChannelEvent",
+			Name: "Local/testChannel",
+		},
+	}
+
+	list := entitiesFromEvent(e)
+	if len(list) != 1 {
+		t.Errorf("Incorrect number of entities: %d != 1", len(list))
+	}
+	if list[0].ID != "testChannelEvent" {
+		t.Errorf("Incorrect channel ID: %s != testChannelEvent", list[0].ID)
+	}
+}

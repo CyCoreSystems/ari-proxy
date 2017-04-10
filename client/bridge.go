4a9da9f26c32162d10ba95ba82124ad00428b012
Seán C McCord <ulexus@gmail.com>
2017-04-10 12:55:37 -0400

    Use client request wrappers for bridge

--- client/bridge.go
+++ client/bridge.go
@@ -3,6 +3,7 @@ package client
 import (
 	"github.com/CyCoreSystems/ari"
 	"github.com/CyCoreSystems/ari-proxy/proxy"
+	uuid "github.com/satori/go.uuid"
 )
 
 type bridge struct {
@@ -10,23 +11,17 @@ type bridge struct {
 }
 
 func (b *bridge) Create(id string, t string, name string) (h ari.BridgeHandle, err error) {
-	req := proxy.Request{
+	resp, err := b.c.getRequest(&proxy.Request{
 		BridgeCreate: &proxy.BridgeCreate{
 			ID:   id,
 			Name: name,
 			Type: t,
 		},
-	}
-	var resp proxy.Response
-	err = b.c.nc.Request(proxy.GetSubject(b.c.prefix, b.c.appName, ""), &req, &resp, b.c.requestTimeout)
+	})
 	if err != nil {
 		return
 	}
-	if err = resp.Err(); err != nil {
-		return
-	}
-	h = b.Get(resp.Entity.ID)
-	return
+	return b.Get(resp.ID), nil
 }
 
 func (b *bridge) Get(id string) ari.BridgeHandle {
@@ -34,18 +29,13 @@ func (b *bridge) Get(id string) ari.BridgeHandle {
 }
 
 func (b *bridge) List() (ret []ari.BridgeHandle, err error) {
-	req := proxy.Request{
+	resp, err := b.c.listRequest(&proxy.Request{
 		BridgeList: &proxy.BridgeList{},
-	}
-	var resp proxy.Response
-	err = b.c.nc.Request(proxy.GetSubject(b.c.prefix, b.c.appName, ""), &req, &resp, b.c.requestTimeout)
+	})
 	if err != nil {
-		return
+		return nil, err
 	}
-	if err = resp.Err(); err != nil {
-		return
-	}
-	for _, i := range resp.EntityList.List {
+	for _, i := range resp.List {
 		ret = append(ret, b.Get(i.ID))
 	}
 	return
@@ -55,122 +45,81 @@ func (b *bridge) Playback() ari.Playback {
 	return b.c.Playback()
 }
 
-func (b *bridge) Data(id string) (d *ari.BridgeData, err error) {
-	req := proxy.Request{
+func (b *bridge) Data(id string) (*ari.BridgeData, error) {
+	resp, err := b.c.dataRequest(&proxy.Request{
 		BridgeData: &proxy.BridgeData{
 			ID: id,
 		},
-	}
-	var resp proxy.DataResponse
-	err = b.c.nc.Request(proxy.GetSubject(b.c.prefix, b.c.appName, ""), &req, &resp, b.c.requestTimeout)
+	})
 	if err != nil {
-		return
+		return nil, err
 	}
-	if err = resp.Err(); err != nil {
-		return
-	}
-	d = resp.BridgeData
-	return
+	return resp.Bridge, nil
 }
 
-func (b *bridge) AddChannel(bridgeID string, channelID string) (err error) {
-	req := proxy.Request{
+func (b *bridge) AddChannel(bridgeID string, channelID string) error {
+	return b.c.commandRequest(&proxy.Request{
 		BridgeAddChannel: &proxy.BridgeAddChannel{
 			ID:      bridgeID,
 			Channel: channelID,
 		},
-	}
-	var resp proxy.Response
-	err = b.c.nc.Request(proxy.CommandSubject(b.c.prefix, b.c.appName, ""), &req, &resp, b.c.requestTimeout)
-	if err != nil {
-		return
-	}
-	if err = resp.Err(); err != nil {
-		return
-	}
-	return
+	})
 }
 
-func (b *bridge) RemoveChannel(bridgeID string, channelID string) (err error) {
-	req := proxy.Request{
+func (b *bridge) RemoveChannel(bridgeID string, channelID string) error {
+	return b.c.commandRequest(&proxy.Request{
 		BridgeRemoveChannel: &proxy.BridgeRemoveChannel{
 			ID:      bridgeID,
 			Channel: channelID,
 		},
-	}
-	var resp proxy.Response
-	err = b.c.nc.Request(proxy.CommandSubject(b.c.prefix, b.c.appName, ""), &req, &resp, b.c.requestTimeout)
-	if err != nil {
-		return
-	}
-	if err = resp.Err(); err != nil {
-		return
-	}
-	return
+	})
 }
 
-func (b *bridge) Delete(id string) (err error) {
-	req := proxy.Request{
+func (b *bridge) Delete(id string) error {
+	return b.c.commandRequest(&proxy.Request{
 		BridgeDelete: &proxy.BridgeDelete{
 			ID: id,
 		},
-	}
-	var resp proxy.Response
-	err = b.c.nc.Request(proxy.CommandSubject(b.c.prefix, b.c.appName, ""), &req, &resp, b.c.requestTimeout)
-	if err != nil {
-		return
-	}
-	if err = resp.Err(); err != nil {
-		return
-	}
-	return
+	})
 }
 
-func (b *bridge) Play(id string, playbackID string, mediaURI string) (h ari.PlaybackHandle, err error) {
-	req := proxy.Request{
+func (b *bridge) Play(id string, playbackID string, mediaURI string) (ari.PlaybackHandle, error) {
+	err := b.c.commandRequest(&proxy.Request{
 		BridgePlay: &proxy.BridgePlay{
 			ID:         id,
 			MediaURI:   mediaURI,
 			PlaybackID: playbackID,
 		},
-	}
-	var resp proxy.Response
-	err = b.c.nc.Request(proxy.CommandSubject(b.c.prefix, b.c.appName, ""), &req, &resp, b.c.requestTimeout)
+	})
 	if err != nil {
-		return
+		return nil, err
 	}
-	if err = resp.Err(); err != nil {
-		return
-	}
-	h = b.c.Playback().Get(resp.Entity.ID)
-	return
+	return b.c.Playback().Get(playbackID), nil
 }
 
-func (b *bridge) Record(id string, name string, opts *ari.RecordingOptions) (h ari.LiveRecordingHandle, err error) {
+func (b *bridge) Record(id string, name string, opts *ari.RecordingOptions) (ari.LiveRecordingHandle, error) {
 	if opts == nil {
 		opts = &ari.RecordingOptions{}
 	}
+	if name == "" {
+		name = uuid.NewV1().String()
+	}
 
-	req := proxy.Request{
+	err := b.c.commandRequest(&proxy.Request{
 		BridgeRecord: &proxy.BridgeRecord{
 			ID:      id,
 			Options: opts,
 			Name:    name,
 		},
-	}
-	var resp proxy.Response
-	err = b.c.nc.Request(proxy.CommandSubject(b.c.prefix, b.c.appName, ""), &req, &resp, b.c.requestTimeout)
+	})
 	if err != nil {
-		return
+		return nil, err
 	}
-	if err = resp.Err(); err != nil {
-		return
-	}
-	h = b.c.LiveRecording().Get(resp.Entity.ID)
-	return
+	return b.c.LiveRecording().Get(name), nil
 }
 
 func (b *bridge) Subscribe(id string, nx ...string) ari.Subscription {
+	// TODO
 	//	ns := newSubscription(b.Get(id))
 	//	ns.Start(b.subscriber, nx...)
 	//	return ns
@@ -202,21 +151,19 @@ func (bh *bridgeHandle) Delete() error {
 	return bh.bridge.Delete(bh.id)
 }
 
-func (bh *bridgeHandle) Data() (bd *ari.BridgeData, err error) {
-	bd, err = bh.bridge.Data(bh.id)
-	return
+func (bh *bridgeHandle) Data() (*ari.BridgeData, error) {
+	return bh.bridge.Data(bh.id)
 }
 
-func (bh *bridgeHandle) Play(playbackID string, mediaURI string) (ph ari.PlaybackHandle, err error) {
-	ph, err = bh.bridge.Play(bh.id, playbackID, mediaURI)
-	return
+func (bh *bridgeHandle) Play(playbackID string, mediaURI string) (ari.PlaybackHandle, error) {
+	return bh.bridge.Play(bh.id, playbackID, mediaURI)
 }
 
-func (bh *bridgeHandle) Record(name string, opts *ari.RecordingOptions) (rh ari.LiveRecordingHandle, err error) {
-	rh, err = bh.bridge.Record(bh.id, name, opts)
-	return
+func (bh *bridgeHandle) Record(name string, opts *ari.RecordingOptions) (ari.LiveRecordingHandle, error) {
+	return bh.bridge.Record(bh.id, name, opts)
 }
 
 func (bh *bridgeHandle) Match(e ari.Event) (ok bool) {
+	// TODO
 	return
 }

d02f80ce2afb9ef78286e57110519f9b3a25995d
Seán C McCord <ulexus@gmail.com>
2017-04-16 19:23:18 -0400

    add client cluster

---
+++ client/cluster/cluster.go
@@ -0,0 +1,99 @@
+package cluster
+
+import (
+	"strings"
+	"sync"
+	"time"
+)
+
+var mu sync.Mutex
+
+// Cluster describes the set of ari proxies in a system.  The list is indexed by a hash of the asterisk ID and the ARI application and indicates the time of last contact.
+type Cluster map[string]time.Time
+
+// hash returns the key for a given proxy instance
+func hash(id, app string) string {
+	return id + "|" + app
+}
+
+// dehash returns the Asterisk ID and ARI application represented by the given key
+func dehash(key string) (id string, app string) {
+	pieces := strings.Split(key, "|")
+	if len(pieces) < 2 {
+		return
+	}
+	return pieces[0], pieces[1]
+}
+
+// Member describes the state of a Member of an application cluster
+type Member struct {
+	// ID is the unique identifier for the Asterisk node
+	ID string
+
+	// App indicates the ARI application of this proxy
+	App string
+
+	// LastActive is the timestamp of the last occurrence of this node
+	LastActive time.Time
+}
+
+// All returns a list of all cluster members whose LastActive time is no older thatn the given maxAge.
+func (c Cluster) All(maxAge time.Duration) (list []Member) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	for k, v := range c {
+		if maxAge == 0 || time.Since(v) < maxAge {
+			id, app := dehash(k)
+			list = append(list, Member{
+				ID:         id,
+				App:        app,
+				LastActive: v,
+			})
+		}
+	}
+	return
+}
+
+// App returns a list of all cluster members for the given ARI Application whose LastActive time is no older than the given maxAge.
+func (c Cluster) App(app string, maxAge time.Duration) (list []Member) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	for k, v := range c {
+		i, a := dehash(k)
+		if app == a && (maxAge == 0 || time.Since(v) < maxAge) {
+			list = append(list, Member{
+				ID:         i,
+				App:        a,
+				LastActive: v,
+			})
+		}
+	}
+	return
+}
+
+// Update adds (or updates) a proxy to/in the cluster
+func (c Cluster) Update(id, app string) {
+	mu.Lock()
+	c[hash(id, app)] = time.Now()
+	mu.Unlock()
+}
+
+// Purge removes any proxies in the cluster which are older than the given maxAge.
+func (c Cluster) Purge(maxAge time.Duration) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	var removalKeys []string
+
+	for k, v := range c {
+		if maxAge == 0 || time.Since(v) > maxAge {
+			removalKeys = append(removalKeys, k)
+		}
+	}
+
+	for _, key := range removalKeys {
+		delete(c, key)
+	}
+}
---
+++ client/cluster/cluster_test.go
@@ -0,0 +1,110 @@
+package cluster
+
+import (
+	"strings"
+	"testing"
+	"time"
+
+	uuid "github.com/satori/go.uuid"
+)
+
+func TestHash(t *testing.T) {
+	id := uuid.NewV1().String()
+	app := uuid.NewV1().String()
+
+	testID, testApp := dehash(hash(id, app))
+	if id != testID {
+		t.Error("Asterisk IDs do not match")
+	}
+	if app != testApp {
+		t.Error("ARI Applications do not match")
+	}
+}
+
+func TestAll(t *testing.T) {
+	c := make(Cluster)
+	c.Update("A1", "TestApp")
+	c.Update("A2", "TestApp")
+
+	list := c.All(0)
+	if len(list) != 2 {
+		t.Errorf("Incorrect number of cluster members: %d != 2", len(list))
+	}
+
+	c.Update("B1", "TestApp2")
+	c.Update("B2", "TestApp2")
+
+	list = c.All(0)
+	if len(list) != 4 {
+		t.Errorf("Incorrect number of cluster members: %d != 4", len(list))
+	}
+}
+
+func TestApp(t *testing.T) {
+	c := make(Cluster)
+	c.Update("A1", "TestApp")
+	c.Update("A2", "TestApp")
+	c.Update("B1", "TestApp2")
+	c.Update("B2", "TestApp2")
+
+	list := c.App("TestApp", 0)
+	if len(list) != 2 {
+		t.Errorf("Incorrect number of cluster members: %d != 2", len(list))
+	}
+	if list[1].App != "TestApp" {
+		t.Errorf("Incorrect app: %s != TestApp", list[0].App)
+	}
+	if !strings.HasPrefix(list[0].ID, "A") {
+		t.Errorf("Incorrect ID: %s does not begin with A", list[1].ID)
+	}
+
+	list = c.App("TestApp2", 0)
+	if len(list) != 2 {
+		t.Errorf("Incorrect number of cluster members: %d != 2", len(list))
+	}
+	if list[0].App != "TestApp2" {
+		t.Errorf("Incorrect app: %s != TestApp2", list[0].App)
+	}
+	if !strings.HasPrefix(list[1].ID, "B") {
+		t.Errorf("Incorrect ID: %s does not begin with B", list[1].ID)
+	}
+}
+
+func TestPurge(t *testing.T) {
+	c := make(Cluster)
+	c.Update("A1", "TestApp")
+	c.Update("A2", "TestApp")
+	c.Update("B1", "TestApp2")
+	c.Update("B2", "TestApp2")
+
+	list := c.All(0)
+	if len(list) != 4 {
+		t.Errorf("Incorrect number of cluster members: %d != 4", len(list))
+	}
+
+	c.Purge(50 * time.Millisecond)
+
+	list = c.All(0)
+	if len(list) != 4 {
+		t.Errorf("Incorrect number of cluster members: %d != 4", len(list))
+	}
+
+	time.Sleep(50 * time.Millisecond)
+
+	c.Update("B1", "TestApp2")
+
+	c.Purge(45 * time.Millisecond)
+
+	list = c.All(0)
+	if len(list) != 1 {
+		t.Errorf("Incorrect number of cluster members: %d != 1", len(list))
+	}
+
+	c.Update("A2", "TestApp")
+
+	list = c.All(0)
+	if len(list) != 2 {
+		t.Errorf("Incorrect number of cluster members: %d != 2", len(list))
+	}
+
+}

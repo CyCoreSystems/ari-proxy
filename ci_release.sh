3fab0cc521647bf14fb9387761643194f30f8ae1
Seán C McCord <ulexus@gmail.com>
2018-04-22 16:00:15 -0400

    fix ci_release script

--- ci_release.sh
+++ ci_release.sh
@@ -1,2 +1,10 @@
 #!/bin/bash
+
+# Login to Docker
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
+
+# Install goreleaser
+go get -u github.com/goreleaser/goreleaser
+
+# Create the release
+goreleaser release

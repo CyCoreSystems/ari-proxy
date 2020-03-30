7e0fa5482b1e58667db61e60ee98a88e2bafd920
Vitor Espindola <vitor.espindola@byne.com.br>
2020-03-30 15:53:59 -0300

    Ignore nil values on dialogs for event listing
    Fix #28

--- server/events.go
+++ server/events.go
@@ -4,6 +4,9 @@ import "github.com/CyCoreSystems/ari/v5"
 
 func (s *Server) dialogsForEvent(e ari.Event) (ret []string) {
 	for _, k := range e.Keys() {
+		if k == nil {
+			continue
+		}
 		ret = append(ret, s.Dialog.List(k.Kind, k.ID)...)
 	}
 	return

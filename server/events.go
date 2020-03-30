e916660921b23489080b094e203dfc4cbec410a0
Seán C McCord <ulexus@gmail.com>
2020-03-30 16:27:36 -0400

    events without keys should emit warning log

--- server/events.go
+++ server/events.go
@@ -5,7 +5,7 @@ import "github.com/CyCoreSystems/ari/v5"
 func (s *Server) dialogsForEvent(e ari.Event) (ret []string) {
 	for _, k := range e.Keys() {
 		if k == nil {
-			s.Log.Debug("received nil key for event", "event", e)
+			s.Log.Warn("received nil key for event", "event", e)
 			continue
 		}
 		ret = append(ret, s.Dialog.List(k.Kind, k.ID)...)
